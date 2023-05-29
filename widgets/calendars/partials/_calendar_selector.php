f280ca912e2d00a0b4fd5b11be64231d5f1e5dd1
sanchez <sanchez@acornassociated.org>
2023-05-29 17:50:52 +0100

    No friends

--- widgets/calendars/partials/_calendar_selector.php
+++ widgets/calendars/partials/_calendar_selector.php
@@ -29,28 +29,33 @@ print('<ul class="layout-row">');
 $instances = Instance::where('instance_start', '>=', $date)
     ->where('instance_start', '<=', $enddate)
     ->get();
-foreach ($instances as $instance) {
-    $id        = $instance->id;
-    $eventPart = &$instance->eventPart;
-    $for       = "instance-$id";
-    $start     = $instance->instance_start->format('H:i');
-    $checked   = ($value && in_array($id, $value) ? "checked='1'" : '');
-    print("<li><input id='$for' name='instances[]' $checked value='$instance->id' type='checkbox'></input>&nbsp;<label for='$for'>$start $eventPart->name</label></li>");
-}
 
-/* TODO: Use day partial
-while ($date < $enddate) {
-    print($this->makePartial('day', [
-        'date'    => $date,
-        'title'   => $date->format('d'),
-        'format'  => 'd',
-        'classes' => array(),
-        'styles'  => array(),
-        'range'   => 'in',
-        'events'  => $instances,
-        'columns' => $columns,
-    ]));
-    $date->add(1, 'day');
+if (count($instances)) {
+    foreach ($instances as $instance) {
+        $id        = $instance->id;
+        $eventPart = &$instance->eventPart;
+        $for       = "instance-$id";
+        $start     = $instance->instance_start->format('H:i');
+        $checked   = ($value && in_array($id, $value) ? "checked='1'" : '');
+        print("<li><input id='$for' name='instances[]' $checked value='$instance->id' type='checkbox'></input>&nbsp;<label for='$for'>$start $eventPart->name</label></li>");
+    }
+
+    /* TODO: Use day partial
+    while ($date < $enddate) {
+        print($this->makePartial('day', [
+            'date'    => $date,
+            'title'   => $date->format('d'),
+            'format'  => 'd',
+            'classes' => array(),
+            'styles'  => array(),
+            'range'   => 'in',
+            'events'  => $instances,
+            'columns' => $columns,
+        ]));
+        $date->add(1, 'day');
+    }
+        */
+    print('</ul>');
+} else {
+    print($this->makePartial('hint_calendar_selector_empty'));
 }
-    */
-print('</ul>');
---
+++ widgets/calendars/partials/_hint_calendar_selector_empty.php
@@ -0,0 +1,11 @@
+<div class="layout-row min-size">
+    <div class="callout">
+        <div class="header">
+            <i class="icon-info"></i>
+            <h3><?= e('There are no relevant events.') ?></h3>
+            <p>
+                <?= e(trans('Go to the')) . ' <a target="_blank" href="/backend/acornassociated/calendar/calendar">Calendar</a> ' . e(trans('to create one.')) ?>
+            </p>
+        </div>
+    </div>
+</div>
