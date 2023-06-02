f41d1941d4923caf89f9ca043e2004111fc4c39d
anewholm <annesley_newholm@yahoo.it>
2023-06-02 15:58:33 +0100

    laravel-websockets

---
+++ events/EventNew.php
@@ -0,0 +1,41 @@
+<?php namespace AcornAssociated\Calendar\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+
+use AcornAssociated\Calendar\Models\EventPart;
+
+class EventNew implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $eventPart;
+
+    public function __construct(EventPart $_eventPart)
+    {
+        $this->eventPart = $_eventPart;
+    } 
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+        return [
+            new Channel('calendar')
+        ];
+    }
+
+    public function broadcastAs()
+    {
+        return 'event.new';
+    }
+}
--- widgets/calendars/partials/_calendar.php
+++ widgets/calendars/partials/_calendar.php
@@ -1,4 +1,4 @@
-<div id="Calendars" class="calendar-container" websocket-listen websocket-oncalendar-update="'calendar': '#Calendars'">
+<div id="Calendars" class="calendar-container" websocket-listen="calendar" websocket-oncalendar-update="'calendar': '#Calendars-instance'">
     <div class="layout calendar sortable" data-request-drop="onChangeDate">
         <?php if (count($weeks)) print($this->makePartial('calendar_header', ['week' => $weeks[0]])) ?>
         <?php foreach ($weeks as $w => $week) print($this->makePartial('week', ['week' => $week])); ?>
