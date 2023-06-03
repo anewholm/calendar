1e23ea4a05b1244df07b2e7bdabe41050efd952b
anewholm <annesley_newholm@yahoo.it>
2023-06-03 17:55:43 +0100

    calendar:run skeleton

---
+++ console/RunCommand.php
@@ -0,0 +1,37 @@
+<?php namespace AcornAssociated\Calendar\Console;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Backend\Models\User;
+use AcornAssociated\Calendar\Models\EventPart;
+
+class RunCommand extends Command
+{
+    protected $name = 'calendar:run';
+    protected $description = 'Runs the reminder alarms.';
+
+    public function handle()
+    {
+        $verbose = $this->option('verbose');
+        $this->info("Reminder notifications running");
+
+        while (TRUE) {
+            foreach (EventPart::whereNotNull('alarm') as $eventPart) {
+                $this->info($eventPart->name);
+            }
+            sleep(5);
+        }
+    }
+
+    protected function getArguments()
+    {
+        return [];
+    }
+
+    protected function getOptions()
+    {
+        return [];
+    }
+
+}
